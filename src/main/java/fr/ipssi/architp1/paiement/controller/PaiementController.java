f08e969af890435d1f7ff07ba969f798afc514ee
Antoine Lecoffre <antoine.lecoffre@osmose-solutions.com>
2022-12-15 11:43:52 +0100

    Adding Paiement controller

--- src/main/java/fr/ipssi/architp1/paiement/controller/PaiementController.java
+++ src/main/java/fr/ipssi/architp1/paiement/controller/PaiementController.java
@@ -1,4 +1,4 @@
-package fr.ipssi.architp1.paiement.rest;
+package fr.ipssi.architp1.paiement.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,13 +7,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/about")
-public class AboutController {
-
-
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+public class PaiementController {
+    @RequestMapping(value = "/paiement", method = RequestMethod.GET)
     public ResponseEntity<String> hello() {
         return new ResponseEntity<>("Bonjour", HttpStatus.OK);
     }
-
 }
+
